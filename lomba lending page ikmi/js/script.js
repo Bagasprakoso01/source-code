6f46532cb9f3bc61416ee4355e07a3eeef833601
Bagasprakoso01 <57170941+Bagasprakoso01@users.noreply.github.com>
2023-08-20 10:17:02 +0700

    Add files via upload

---
+++ lomba lending page ikmi/js/script.js
@@ -0,0 +1,11 @@
+// JavaScript code can be added here to enhance functionality as needed.
+
+// Example: Add a click event listener to the "Explore More" button.
+document.addEventListener('DOMContentLoaded', function () {
+    const exploreButton = document.querySelector('.btn');
+
+    exploreButton.addEventListener('click', function () {
+        // Replace this with the desired functionality.
+        alert('Wonderfull Indonesia tetap berkembang dan maju tersu bisnis wisata indonesia, Indonesia tetap no 1');
+    });
+});
